d9ad1e3aec889722c35907171c69c1562c5b32ce
koren85 <mailbobik@gmail.com>
2025-02-01 12:11:29 +0300

    add readme

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_time_entries_group_by_issue do
   name 'Redmine Time Entries Group by Issue Plugin'
-  author 'Ваше Имя'
-  description 'Добавляет возможность группировки трудозатрат по задачам в отчётах'
+  author 'Alex Chern'
+  description 'Adds the ability to group labor costs by task in reports'
   version '1.0.0'
   requires_redmine version_or_higher: '4.1.0'
 end
